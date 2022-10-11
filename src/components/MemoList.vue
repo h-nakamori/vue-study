73137c359a74cc1d19d582c6b2bbc7eb0eee0068
h-nakamori <115455235+h-nakamori@users.noreply.github.com>
2022-10-11 18:24:56 +0900

    リストをコンポーネント化するための土台作成

---
+++ src/components/MemoList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="memo-container">
+    <!-- <p v-if="this.id == 0" class="no-memos">No memos has been posted</p>
+    <ul v-if="this.id != 0" class="ul">
+      <li v-for="memo in memos" :key="memo.id" class="memo-list">
+        <span>{{ memo.description }}</span>
+        <button @click="deletePost(memo.id)" class="delete-button">
+          DELETE
+        </button>
+      </li>
+    </ul> -->
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+@Component
+export default class MemoList extends Vue {
+}
+</script>
+
+<style scoped>
+</style>
